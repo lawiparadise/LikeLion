3dae0f8cca7a5378e4b4475677bdad70078be52c
lawiparadise <lawiparadise@gmail.com>
2025-02-21 17:48:31 +0900

    TodayTask1

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDCHANGE = 0x02;
+        static void SetWallpaper(string imagePath)
+        {
+            // 바탕화면 변경
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, imagePath, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+            Console.WriteLine("바탕화면이 변경되었습니다.");
+        }
+
+        static void Main(string[] args)
+        {
+          
+            // 로딩바 시작화면
+            // 게임스토리1
+
+            Console.WriteLine("엔터를 치면 게임이 시작됩니다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("이것은 공포게임이며 어린이, 임산부, 노약자, 심약자는 주의하시기 바랍니다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 지금 집에 있습니까? y or n");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 성인입니까? y or n");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 지금 혼자 있습니까? y or n");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 종종 우울합니까? y or n");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 종종 우울합니까? y or n");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 종종 우울합니까? y or n");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 이 게임이 무엇인지 궁금합니까? y or n");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 신이 존재함을 믿습니까? y or n");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 신이 선하다고 생각합니까? y or n");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 불우 이웃을 돕습니까? y or n");
+            String a = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신의 컴퓨터는 곧 랜섬웨어 감염됩니다..3");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("당신의 컴퓨터는 곧 랜섬웨어 감염됩니다..2");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("당신의 컴퓨터는 곧 랜섬웨어 감염됩니다..1");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            string imagePath = @"C:\Users\xnTjazp2zm\Documents\b.png";
+            SetWallpaper(imagePath);
+
+            if (a == "y")
+            {
+                while(true)
+                {
+                    Console.WriteLine("게임이 끝났습니다.");
+                }
+            }
+            else
+            {
+                while (true)
+                {
+                    Console.WriteLine("게임이 끝났습니다.");
+                }
+            }
+            
+        }
+    }
+}
---
+++ studyReadLine/studyReadLine/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("이름을 입력하세요.");
+            //string userName = Console.ReadLine();
+            //Console.WriteLine($"안녕하세요, {userName}");
+
+            //Console.Write("나이를 입력하세요.");
+            //string input = Console.ReadLine();
+            //int age = int.Parse(input);
+
+            //Console.WriteLine($"내년에는 {age++}살이 되겠군요!");
+            //Console.WriteLine($"내년에는 {age}살이 되겠군요!");
+
+            Console.Write($"루인 스킬 피해 입력 : ");
+            string a = Console.ReadLine();
+            float b = float.Parse(a);
+            Console.WriteLine($"루인 스킬 피해 : {b} %");
+            Console.WriteLine("");
+
+            Console.Write($"카드 게이지 획득량 입력 : ");
+            a = Console.ReadLine();
+            b = float.Parse(a);
+            Console.WriteLine($"카드 게이지 획득량 : {b} %");
+            Console.WriteLine("");
+
+            Console.Write($"각성기 피해 입력 : ");
+            a = Console.ReadLine();
+            b = float.Parse(a);
+            Console.WriteLine($"각성기 피해 : {b} %");
+            Console.WriteLine("");
+
+            Console.Write($"최대 마나 입력 : ");
+            a = Console.ReadLine();
+            b = float.Parse(a);
+            Console.WriteLine($"최대 마나 : {b}");
+            Console.WriteLine("");
+
+            Console.Write($"전투 중 마나 회복량 입력 : ");
+            a = Console.ReadLine();
+            b = float.Parse(a);
+            Console.WriteLine($"전투 중 마나 회복량 : {b}");
+            Console.WriteLine("");
+
+            Console.Write($"비전투 중 마나 회복량 입력 : ");
+            a = Console.ReadLine();
+            b = float.Parse(a);
+            Console.WriteLine($"비전투 중 마나 회복량 : {b}");
+            Console.WriteLine("");
+
+            Console.Write($"이동 속도 : ");
+            a = Console.ReadLine();
+            b = float.Parse(a);
+            Console.WriteLine($"이동 속도 : {b:F1} %");
+            Console.WriteLine("");
+
+            Console.Write($"탈 것 속도 : ");
+            a = Console.ReadLine();
+            b = float.Parse(a);
+            Console.WriteLine($"탈 것 속도 : {b:F1} %");
+            Console.WriteLine("");
+
+            Console.Write($"운반 속도 : ");
+            a = Console.ReadLine();
+            b = float.Parse(a);
+            Console.WriteLine($"운반 속도 : {b:F1} %");
+            Console.WriteLine("");
+
+            Console.Write($"스킬 재사용 대기시간 감소 : ");
+            a = Console.ReadLine();
+            b = float.Parse(a);
+            Console.WriteLine($"스킬 재사용 대기시간 감소 : {b:F1} %");
+            Console.WriteLine("");
+        }
+    }
+}
