d441a573d231c5663d67ec88066075417aebfd2f
lawiparadise <lawiparadise@gmail.com>
2025-02-21 15:04:24 +0900

    loading

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□");
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine("■□□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("■■■□□□□□");
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+    }
+}
